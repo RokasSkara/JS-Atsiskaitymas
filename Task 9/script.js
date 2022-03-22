806edf9947f26ca41108480279d2de540f414640
RokasSkara <rokas.skarakodas@gmail.com>
2022-03-22 10:31:02 +0200

    task 9 - done

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,17 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive = () => (this.budget > 100000000)
+}
+
+let spoderman = new Movie('Spoderman', 'Director', 99999999)
+console.log(spoderman.wasExpensive())
+let TheAmazingspoderman = new Movie('TheAmazingspoderman', 'Director', 199999999)
+console.log(TheAmazingspoderman.wasExpensive())
