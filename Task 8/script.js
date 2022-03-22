28bdc6289d4ca5c2c84556832dad39c5ae348079
RokasSkara <rokas.skarakodas@gmail.com>
2022-03-22 10:26:46 +0200

    task 8 - done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,16 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    sum = (a, b) => (a + b)
+    subtraction = (a, b) => (a - b)
+    multiplication = (a, b) => (a * b)
+    division = (a, b) => (a / b)
+}
+
+let test = new Calculator()
+console.log('Sum ', test.sum(2, 3))
+console.log('Subtraction ', test.subtraction(2, 3))
+console.log('Multiplication ', test.multiplication(2, 3))
+console.log('Division ', test.division(2, 3))
