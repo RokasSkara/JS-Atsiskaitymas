5d33d4610d7cdea3945994a65d11bab3df65ed54
RokasSkara <rokas.skarakodas@gmail.com>
2022-03-22 09:49:05 +0200

    task 4 - done

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,29 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+window.onload = () => {
+    fetch(ENDPOINT)
+        .then(res => res.json())
+        .then(data => createCard(data))
+}
+
+let createCard = (data) => {
+    let output = document.querySelector('#output')
+    data.forEach(car => {
+        let div = document.createElement('div')
+        div.classList.add('carCard')
+        let h2 = document.createElement('h2')
+        h2.textContent = car.brand
+        div.append(h2)
+        let ul = document.createElement('ul')
+        car.models.forEach(model => {
+            let li = document.createElement('li')
+            li.textContent = model
+            ul.append(li)
+        })
+        div.append(ul)
+        output.append(div)
+    });
+
+}
